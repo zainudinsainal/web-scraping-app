1f8615cffa69cd21502841ce3af18b74927bbb40
zainudinsainal <supernova37@gmail.com>
2018-02-25 19:49:35 +0800

    update controller

--- app/controllers/tutorial_controller.rb
+++ app/controllers/tutorial_controller.rb
@@ -1,10 +1,15 @@
 class TutorialController < ApplicationController
-  def index
+def index
+    @tutorials = Tutorial.all
   end
 
   def view
+    @tutorial = Tutorial.find params[:id]
   end
 
   def queue
+    TutorialService.new.delay.scrape_listing
+    flash[:message] = 'Scraping queued!'
+    redirect_to :action => :index
   end
 end
