d7feca1fc077c1b76f40fc6bd0f07555dd6d1a58
zainudinsainal <supernova37@gmail.com>
2018-02-25 19:42:37 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'tutorial/index'
+  get '/' => 'tutorial#index'
 
-  get 'tutorial/view'
+  get '/tutorial/:id' => 'tutorial#view'
 
-  get 'tutorial/queue'
+  get '/queue' => 'tutorial#queue'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
